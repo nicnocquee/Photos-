2cf124c05929871c52f4b47dfaec4ccae8eee4a5
Nico Prananta <nicnocquee@yahoo.com>
2014-08-12 15:18:33 +0900

    Initial Commit

---
+++ Photos+/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photos+
+//
+//  Created by  on 8/12/14.
+//  Copyright (c) 2014 Delightful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photos+/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Photos+
+//
+//  Created by  on 8/12/14.
+//  Copyright (c) 2014 Delightful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
